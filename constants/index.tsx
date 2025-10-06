4314213f9530fd0c06cca96f32502b36ada0335f
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-10-06 19:14:18 +0100

    created a reusable menu button

---
+++ constants/index.tsx
@@ -0,0 +1,22 @@
+export const NavLinks = [
+  {
+    id: 1,
+    label: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    label: "Services",
+    href: "/services",
+  },
+  {
+    id: 3,
+    label: "Testimonials",
+    href: "/testimonials",
+  },
+  {
+    id: 4,
+    label: "About",
+    href: "/about",
+  },
+];
