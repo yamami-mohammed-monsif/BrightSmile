90d03fe00823f97091eadcaf85a07dc097dbf195
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-10-06 19:16:07 +0100

    created responsive header with animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <Header />
     </div>
   );
 }
---
+++ components/Header/index.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import MenuButton from "../UI/MenuButton";
+import MobileNav from "./MobileNav";
+import DesktopNav from "./DesktopNav";
+import { CalendarDays } from "lucide-react";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isMenuOpen]);
+
+  // Escape key support
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") setIsMenuOpen(false);
+    };
+    if (isMenuOpen) {
+      document.addEventListener("keydown", handleEsc);
+    }
+    return () => document.removeEventListener("keydown", handleEsc);
+  }, [isMenuOpen]);
+  return (
+    <>
+      <MobileNav isMenuOpen={isMenuOpen} onToggle={handleToggleMenu} />
+
+      <header className="font-nunito-sans">
+        <div className="flex items-center justify-between px-5 md:px-8 lg:px-[100px] py-4">
+          <div>
+            <Link href="/">
+              <img
+                src="/logo.png"
+                className="aspect-square w-11 md:w-14 lg:w-20"
+                alt="logo"
+              />
+            </Link>
+          </div>
+
+          <div className="hidden lg:flex lg:items-center lg:justify-center lg:space-x-6">
+            <DesktopNav />
+            <button className="border flex items-center gap-2 ml-8 px-6 py-3 rounded-full header-cta">
+              <CalendarDays strokeWidth={1.5} />
+              <Link href="/">Book Free Consultation</Link>
+            </button>
+          </div>
+
+          <div className="lg:hidden">
+            <MenuButton
+              isOpen={isMenuOpen}
+              onToggle={handleToggleMenu}
+              ariaLabel="Navigation Menu"
+              size="default"
+              className="text-text focus:outline-none"
+            />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
